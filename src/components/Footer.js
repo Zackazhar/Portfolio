fa59578998f3f230aebe376c5704bd38fca85021
Zack <acin19112007@gmail.com>
2025-09-01 14:15:11 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by zackoooo Behera</h3>
+          <h3>Designed and Developed by Azfar</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SB</h3>
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/zackoooo"
+                //href="https://twitter.com/zackoooo"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
