8995434db76dd8a8367989a3d49f7322bf22ee63
Zack <acin19112007@gmail.com>
2025-10-25 16:14:54 +0800

    lol

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,17 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/zackazhar"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/zackazhar"
+                href="https://www.linkedin.com/in/azfar-syahmi-b2a17528a/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
